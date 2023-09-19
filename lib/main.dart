467eead45b57c2aab4626813f94002404391374b
HafizSp <hafizsp45@gmail.com>
2023-09-19 17:13:05 +0600

    listview-gridview

--- lib/main.dart
+++ lib/main.dart
@@ -1,227 +1,112 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      title: 'Photo Gallery',
+      home: MyHomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  MySnackBar(message, context) {
-    return ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(message),
-        duration: Duration(seconds: 1),
-      ),
-    );
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
-        centerTitle: true,
-        backgroundColor: Colors.green,
+        title: Text('Photo Gallery'),
       ),
-      // body: Scrollbar(
-      //   thickness: 20,
-      //   radius: Radius.circular(20),
-      //   child: ListView(
-      //     scrollDirection: Axis.vertical,
-      //     children: [
-      //       ListTile(
-      //         title: Text('Hello'),
-      //         subtitle: Text('My name is Hafiz'),
-      //         isThreeLine: true,
-      //         leading: Icon(Icons.done),
-      //         trailing: Icon(Icons.delete),
-      //         onTap: () {
-      //           MySnackBar('Success', context);
-      //         },
-      //       ),
-      //       Divider(
-      //         color: Colors.grey,
-      //         endIndent: 30,
-      //         indent: 30,
-      //       ),
-      //       ListTile(
-      //         title: Text('Hello'),
-      //         subtitle: Text('My name is Hafiz'),
-      //         isThreeLine: true,
-      //         leading: Icon(Icons.done),
-      //         trailing: Icon(Icons.delete),
-      //         onTap: () {
-      //           MySnackBar('Success', context);
-      //         },
-      //       ),
-      //       Divider(
-      //         color: Colors.grey,
-      //         endIndent: 30,
-      //         indent: 30,
-      //       ),
-      //       ListTile(
-      //         title: Text('Hello'),
-      //         subtitle: Text('My name is Hafiz'),
-      //         isThreeLine: true,
-      //         leading: Icon(Icons.done),
-      //         trailing: Icon(Icons.delete),
-      //         onTap: () {
-      //           MySnackBar('Success', context);
-      //         },
-      //       ),
-      //       Divider(
-      //         color: Colors.grey,
-      //         endIndent: 30,
-      //         indent: 30,
-      //       ),
-      //       ListTile(
-      //         title: Text('Hello'),
-      //         subtitle: Text('My name is Hafiz'),
-      //         isThreeLine: true,
-      //         leading: Icon(Icons.done),
-      //         trailing: Icon(Icons.delete),
-      //         onTap: () {
-      //           MySnackBar('Success', context);
-      //         },
-      //       ),
-      //       Divider(
-      //         color: Colors.grey,
-      //         endIndent: 30,
-      //         indent: 30,
-      //       ),
-      //       ListTile(
-      //         title: Text('Hello'),
-      //         subtitle: Text('My name is Hafiz'),
-      //         isThreeLine: true,
-      //         leading: Icon(Icons.done),
-      //         trailing: Icon(Icons.delete),
-      //         onTap: () {
-      //           MySnackBar('Success', context);
-      //         },
-      //       ),
-      //       Divider(
-      //         color: Colors.grey,
-      //         endIndent: 30,
-      //         indent: 30,
-      //       ),
-      //       ListTile(
-      //         title: Text('Hello'),
-      //         subtitle: Text('My name is Hafiz'),
-      //         isThreeLine: true,
-      //         leading: Icon(Icons.done),
-      //         trailing: Icon(Icons.delete),
-      //         onTap: () {
-      //           MySnackBar('Success', context);
-      //         },
-      //       ),
-      //       Divider(
-      //         color: Colors.grey,
-      //         endIndent: 30,
-      //         indent: 30,
-      //       ),
-      //       ListTile(
-      //         title: Text('Hello'),
-      //         subtitle: Text('My name is Hafiz'),
-      //         isThreeLine: true,
-      //         leading: Icon(Icons.done),
-      //         trailing: Icon(Icons.delete),
-      //         onTap: () {
-      //           MySnackBar('Success', context);
-      //         },
-      //       ),
-      //       Divider(
-      //         color: Colors.grey,
-      //         endIndent: 30,
-      //         indent: 30,
-      //       ),
-      //       ListTile(
-      //         title: Text('Hello'),
-      //         subtitle: Text('My name is Hafiz'),
-      //         isThreeLine: true,
-      //         leading: Icon(Icons.done),
-      //         trailing: Icon(Icons.delete),
-      //         onTap: () {
-      //           MySnackBar('Success', context);
-      //         },
-      //       ),
-      //       Divider(
-      //         color: Colors.grey,
-      //         endIndent: 30,
-      //         indent: 30,
-      //       ),
-      //       ListTile(
-      //         title: Text('Hello'),
-      //         subtitle: Text('My name is Hafiz'),
-      //         isThreeLine: true,
-      //         leading: Icon(Icons.done),
-      //         trailing: Icon(Icons.delete),
-      //         onTap: () {
-      //           MySnackBar('Success', context);
-      //         },
-      //       ),
-      //       Divider(
-      //         color: Colors.grey,
-      //         endIndent: 30,
-      //         indent: 30,
-      //       ),
-      //       ListTile(
-      //         title: Text('Hello'),
-      //         subtitle: Text('My name is Hafiz'),
-      //         isThreeLine: true,
-      //         leading: Icon(Icons.done),
-      //         trailing: Icon(Icons.delete),
-      //         onTap: () {
-      //           MySnackBar('Success', context);
-      //         },
-      //       ),
-      //     ],
-      //   ),
-      // ),
-      body: Scrollbar(
-        thickness: 20,
-        radius: Radius.circular(20),
-        child: GridView.count(
-          crossAxisCount: 3,
-          mainAxisSpacing: 10,
-          crossAxisSpacing: 10,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Icon(Icons.add),
-            Icon(Icons.add),
-            Icon(Icons.add),
-            Icon(Icons.add),
-            Icon(Icons.add),
-            Icon(Icons.add),
-            Icon(Icons.add),
-            Icon(Icons.add),
-            Icon(Icons.add),
-            Icon(Icons.add),
-            Icon(Icons.add),
-            Icon(Icons.add),
-            Icon(Icons.add),
-            Icon(Icons.add),
-            Icon(Icons.add),
-            Icon(Icons.add),
-            Icon(Icons.add),
-            Icon(Icons.add),
-            Icon(Icons.add),
-            Icon(Icons.add),
-            Icon(Icons.add),
-            Icon(Icons.add),
-            Icon(Icons.add),
-            Icon(Icons.add),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Welcome to the Photo Gallery App!',
+                style: TextStyle(fontSize: 24.0),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Enter a message',
+                ),
+              ),
+            ),
+            GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 4.0,
+                mainAxisSpacing: 4.0,
+              ),
+              itemCount: 6,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Image ${index + 1} clicked!'),
+                      ),
+                    );
+                  },
+                  child: Card(
+                    child: Column(
+                      children: [
+                        Image.network(
+                          'https://via.placeholder.com/150',
+                          height: 100,
+                          width: 150,
+                          fit: BoxFit.cover,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text('Image ${index + 1}'),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage('https://via.placeholder.com/50'),
+              ),
+              title: Text('Sample Photo 1'),
+              subtitle: Text('Subtitle 1'),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage('https://via.placeholder.com/50'),
+              ),
+              title: Text('Sample Photo 2'),
+              subtitle: Text('Subtitle 2'),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage('https://via.placeholder.com/50'),
+              ),
+              title: Text('Sample Photo 3'),
+              subtitle: Text('Subtitle 3'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Photos Uploaded Successfully!'),
+                  ),
+                );
+              },
+              child: Text('Upload Photos'),
+            ),
           ],
         ),
       ),
